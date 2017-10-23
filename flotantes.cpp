d9120e0d7ad5ab186e0378bb463a486fd6cb0dae
ccmarcos <marcosantonio199189@gmail.com>
2017-10-22 21:44:08 -0500

    standard-deviation

---
+++ flotantes.cpp
@@ -0,0 +1,47 @@
+#include <iostream> //para funciones principales como imprimir
+#include <fstream> //para el manejo de archivos
+#include <math.h> //para funciones matematicas
+#include <stdlib.h> //para exit()
+using namespace std;
+
+int main(){
+	int count=0;	
+	float sum=0, sum2=0, mean, variance, standard_deviation;
+	string word;
+
+	
+	ifstream infile;
+	infile.open("cadenas.txt");
+
+	while(infile >> word){
+		count++;
+	}
+	
+	infile.close();
+
+	double number[count];
+
+	infile.open("cadenas.txt");
+
+	for(int i=0; i<count; i++)
+    {
+      infile >>  number[i]; // Reading from the file
+      printf("%lf\n",number[i]);
+    }
+
+	infile.close();
+
+	for(int i=0; i<count; i++)
+		sum += number[i];
+	mean = (float)sum/count;
+
+	for(int i=0; i<count; i++)
+		sum2 += (float)pow(number[i]-mean,2);
+	variance = (float)sum2/(count-1);
+	standard_deviation = sqrt(variance);
+
+	printf("mean = %.4f\n", mean);
+    printf("standard deviation = %.4f\n", standard_deviation);
+	
+    return 0;
+}
